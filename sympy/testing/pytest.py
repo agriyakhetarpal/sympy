ee2b1c5c21029f993de6e90f22178d89c91d2072
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-10-15 01:48:06 +0530

    Better way to check if running under Pyodide

--- sympy/testing/pytest.py
+++ sympy/testing/pytest.py
@@ -1,5 +1,6 @@
 """py.test hacks to support XFAIL/XPASS"""
 
+import platform
 import sys
 import re
 import functools
@@ -380,17 +381,12 @@ def warns_deprecated_sympy():
 
 
 def _running_under_pyodide():
-    """Test if running under pyodide."""
-    try:
-        import pyodide_js  # type: ignore  # noqa
-    except ImportError:
-        return False
-    else:
-        return True
+    """Test if running under Pyodide/WASM."""
+    return sys.platform == 'emscripten' or platform.machine() in ["wasm32", "wasm64"]
 
 
 def skip_under_pyodide(message):
-    """Decorator to skip a test if running under pyodide."""
+    """Decorator to skip a test if running under Pyodide/WASM."""
     def decorator(test_func):
         @functools.wraps(test_func)
         def test_wrapper():
